e00f10e71e30eafd34cec68974488bc0fde659b9
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-08-16 10:35:54 +0530

    Add initial connect.php and user.php files with basic structure

---
+++ user.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.rtl.min.css">
+    <title>CRUD</title>
+</head>
+
+<body>
+    <div class="container my-5" >
+        <form>
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Enter your name" autocomplete="off">
+            </div>
+           <div class="mb-3">
+                <label for="email" class="form-label">email</label>
+                <input type="email" class="form-control" id="email" placeholder="Enter your email" autocomplete="off">
+            </div>
+            <div class="mb-3">
+                <label for="mobile" class="form-label">Mobile</label>
+                <input type="text" class="form-control" id="mobile" placeholder="Enter your mobile number" autocomplete="off">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="text" class="form-control" id="password"placeholder="Enter your password"autocomplete="off">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+
+
+    </div>
+
+</body>
+
+</html>
